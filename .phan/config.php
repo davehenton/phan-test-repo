275fc4754b3698972f4a61d08b7fe8802fe45410
Dave Henton <dave@codeclimate.com>
2019-03-08 14:24:49 -0600

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -14,7 +14,7 @@ return [
     // Thus, both first-party and third-party code being used by
     // your application should be included in this list.
     'directory_list' => [
-        'lib',
+        'test/lib',
     ],
 
     // A directory list that defines files that will be excluded
